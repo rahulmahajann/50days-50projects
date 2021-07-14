2a4bc03492594c1ea9f65f66f7c8fb156f61d3cf
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-07-14 12:31:09 +0530

    random commit

--- P-3_rotating_nav/index.js
+++ P-3_rotating_nav/index.js
@@ -1,11 +1,36 @@
 const open = document.querySelector('.open')
 const close = document.querySelector('.close')
+const skills = document.querySelector('.skills')
+const about = document.querySelector('.about')
+const connect = document.querySelector('.connect')
+const achievement = document.querySelector('.achievement')
 const main_box = document.querySelector('.main_box')
 
+// adding the event listener for the mouse click for opening
+// the nav bar
 open.addEventListener('click', () =>{
     main_box.classList.add('show-nav')
 }) 
 
+// adding the event listener for the mouse click for closing
+// the nav bar
 close.addEventListener('click', () =>{
     main_box.classList.remove('show-nav')
 }) 
+
+skills.addEventListener('click', () =>{
+    main_box.classList.remove('show-nav')
+})
+
+about.addEventListener('click', () =>{
+    main_box.classList.remove('show-nav')
+})
+
+connect.addEventListener('click', () =>{
+    main_box.classList.remove('show-nav')
+})
+
+achievement.addEventListener('click', () =>{
+    main_box.classList.remove('show-nav')
+})
+
